ba2f1a8f7341308814ca11c1b3c8a67a3aa315f1
まさたか <shinonome419@icloud.com>
2022-08-18 17:54:58 +0900

    add: Implement NavigationPage UI
    -Implemented NavigationPage(), and its navigation function.
    -Navigation pages are set to be navigated to MyHomePage() for now.
    -I'll implement those pages such as CalenderPage(), UserPage(), and SettingsPage().

---
+++ lib/presentation/navigation_page/navigation_page.dart
@@ -0,0 +1,41 @@
+import 'package:expressive_writing/main.dart';
+import 'package:flutter/material.dart';
+
+class NavigationPage extends StatefulWidget {
+  const NavigationPage({Key? key}) : super(key: key);
+
+  @override
+  State<NavigationPage> createState() => _NavigationPageState();
+}
+
+class _NavigationPageState extends State<NavigationPage> {
+  int selectedPage = 0;
+  final pageItems = [
+    const MyHomePage(),
+    const MyHomePage(),
+    const MyHomePage(),
+  ];
+
+  void onPageSelection(int index) {
+    setState(() {
+      selectedPage = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Expressive Writing")),
+      body: const Center(child: Text("Bottom Navigation")),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month), label: "Diary"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "User"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: "Settings"),
+        ],
+      ),
+    );
+  }
+}
