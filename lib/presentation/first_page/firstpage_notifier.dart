4c95a942e577fa6333521bfc8951d1158ca77703
まさたか <shinonome419@icloud.com>
2022-10-06 15:47:04 +0900

    add: add FirstPageNotifier

--- lib/presentation/first_page/firstpage_notifier.dart
+++ lib/presentation/first_page/firstpage_notifier.dart
@@ -1,1 +1,19 @@
+import 'package:expressive_writing/infrastructure/auth_repository.dart';
+import 'package:expressive_writing/state/firstpage_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class FirstPageNotifier extends StateNotifier<FirstPageState> {
+  final BaseAuthRepository _authRepository;
+
+  FirstPageNotifier({required BaseAuthRepository authRepository})
+      : _authRepository = authRepository,
+        super(FirstPageState(email: "", password: "", userName: ""));
+
+  void setEmail(String email) {
+    state = state.copyWith(email: email);
+  }
+
+  void setPassword(String password) {
+    state = state.copyWith(password: password);
+  }
+}
