d4ac5b90e34847ca135796301a3860e1830ac298
まさたか <shinonome419@icloud.com>
2022-10-07 20:25:03 +0900

    add: update UserPage
    Updated UserPage from StatefulWidget to HookConsumerWidget.

--- lib/presentation/user_page/user_page.dart
+++ lib/presentation/user_page/user_page.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'userpage_notifier.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class UserPage extends StatelessWidget {
+class UserPage extends HookConsumerWidget {
   const UserPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(userPageNotifierProvider);
+    final notifier = ref.read(userPageNotifierProvider.notifier);
+    useEffect(() {
+      Future<void> fetchUser() async {
+        await notifier.fetch();
+      }
+
+      fetchUser();
+      return null;
+    });
     return Scaffold(
       body: Column(
-        children: const [
-          SizedBox(
+        children: [
+          const SizedBox(
             height: 60,
             child: Center(
                 child: Text(
@@ -16,18 +29,18 @@ class UserPage extends StatelessWidget {
               style: TextStyle(fontSize: 15),
             )),
           ),
-          Divider(),
+          const Divider(),
           ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Guest'),
-              subtitle: Text('User level：1')),
-          Divider(),
-          ListTile(title: Text('Diaries：0')),
-          Divider(),
-          ListTile(title: Text('Diary letters：0')),
-          Divider(),
-          ListTile(title: Text('Letters until next level： 100')),
-          Divider(),
+              leading: const Icon(Icons.person),
+              title: Text(state.user.userName),
+              subtitle: Text('User level：${state.user.userLevel}')),
+          const Divider(),
+          ListTile(title: Text('Diaries：${state.user.diaryNumbers}')),
+          const Divider(),
+          ListTile(title: Text('Diary letters：${state.user.diaryLetters}')),
+          const Divider(),
+          const ListTile(title: Text('Letters until next level： 100')),
+          const Divider(),
         ],
       ),
     );
