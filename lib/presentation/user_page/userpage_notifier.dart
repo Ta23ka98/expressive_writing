e4e453cd895b90b4fbb135e1e4b17bc82c5c688f
まさたか <shinonome419@icloud.com>
2022-10-07 19:58:09 +0900

    add: add UserPageNotifier

--- lib/presentation/user_page/userpage_notifier.dart
+++ lib/presentation/user_page/userpage_notifier.dart
@@ -1,1 +1,15 @@
+import 'package:expressive_writing/state/userpage_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:expressive_writing/infrastructure/auth_repository.dart';
+import 'package:expressive_writing/infrastructure/user_repository.dart';
+
+class UserPageNotifier extends StateNotifier<UserPageState> {
+  final BaseAuthRepository _authRepository;
+  final UserRepository _userRepository;
+  UserPageNotifier(
+      {required BaseAuthRepository authRepository,
+      required UserRepository userRepository})
+      : _authRepository = authRepository,
+        _userRepository = userRepository,
+        super(UserPageState.initial());
+}
