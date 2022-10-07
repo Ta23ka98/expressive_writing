b365c6c560a32a58e25ef5a682c99b3d5386def5
まさたか <shinonome419@icloud.com>
2022-10-07 11:12:11 +0900

    fix: fix _jsonFromSnapshot and add userRepositoryProvider

--- lib/infrastructure/user_repository.dart
+++ lib/infrastructure/user_repository.dart
@@ -1,19 +1,42 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:logger/logger.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:expressive_writing/domain/entity/user.dart';
+
+final userRepositoryProvider = Provider((ref) => UserRepository()..init());
 
 class UserRepository {
   final _db = FirebaseFirestore.instance;
   late final CollectionReference _userRef;
 
-  Future<void> create() async {}
+  void init() {
+    _userRef = _db.collection("Users");
+  }
 
-  Future<void> update() async {}
+  Future<User> findById({required String id}) async {
+    final doc = await _userRef.doc(id).get();
+    return User.fromJson(_jsonFromSnapshot(doc));
+  }
 
-  Future<void> delete() async {}
+  Future<void> create({required User user}) async {
+    await _userRef.doc(user.id).set(user.toJson());
+  }
+
+  Future<void> update({required User user}) async {
+    await _userRef.doc(user.id).update(user.toJson());
+  }
+
+  Future<void> delete({required User user}) async {
+    final docRef = _userRef.doc(user.id);
+    await docRef.delete();
+  }
 
   Map<String, dynamic> _jsonFromSnapshot<T extends DocumentSnapshot>(T json) {
-    return {};
+    return {
+      "id": json.id,
+      "userName": json["userName"],
+      "userLevel": json["userLevel"],
+      "diaryLetters": json["password"],
+      "diaryNumbers": json["diaryNumbers"],
+    };
   }
 }
