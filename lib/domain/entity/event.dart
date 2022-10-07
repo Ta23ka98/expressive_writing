feb400a6ee741185d2b32f749fcd2a9c78912248
まさたか <shinonome419@icloud.com>
2022-10-07 13:16:27 +0900

    clean: remove unnecessary codes

--- lib/domain/entity/event.dart
+++ lib/domain/entity/event.dart
@@ -17,37 +17,3 @@ class Event with _$Event {
 
   factory Event.fromJson(Map<String, dynamic> json) => _$EventFromJson(json);
 }
-
-// import 'package:cloud_firestore/cloud_firestore.dart';
-//
-// class Event {
-//   final String title;
-//   final int? wordCount;
-//   final Timestamp? createdAt;
-//
-//   Event(
-//       {required this.title, required this.wordCount, required this.createdAt});
-//
-//   Event.fromJson(Map<String, dynamic> json)
-//       : this(
-//           title: json['description'],
-//           wordCount: json['wordCount'] ?? 0,
-//           createdAt: json['createdAt'] ?? Timestamp.fromDate(DateTime.now()),
-//         );
-//
-//   Map<String, Object?> toJson() {
-//     return {
-//       'title': title,
-//       'createdAt': createdAt, //DartのDateTimeからFirebaseのTimestampへ変換
-//       //'deletedAt': deletedTimestamp
-//     };
-//   }
-// }
-
-//
-// class Event {
-//   final String title;
-//   const Event({required this.title});
-//   @override
-//   String toString() => title;
-// }
