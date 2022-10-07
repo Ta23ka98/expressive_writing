7b0182d19970fc39d9d6f6486c795c1c04618490
まさたか <shinonome419@icloud.com>
2022-10-07 20:00:10 +0900

    add: add UserPageNotifierProvider

--- lib/presentation/user_page/userpage_notifier.dart
+++ lib/presentation/user_page/userpage_notifier.dart
@@ -3,6 +3,12 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:expressive_writing/infrastructure/auth_repository.dart';
 import 'package:expressive_writing/infrastructure/user_repository.dart';
 
+final userPageNotifierProvider = Provider((ref) {
+  return UserPageNotifier(
+      authRepository: ref.read(authRepositoryProvider),
+      userRepository: ref.read(userRepositoryProvider));
+});
+
 class UserPageNotifier extends StateNotifier<UserPageState> {
   final BaseAuthRepository _authRepository;
   final UserRepository _userRepository;
