3592f7d3228568d1ebb52b21c43dbe4fd5012bcf
まさたか <shinonome419@icloud.com>
2022-10-13 16:37:49 +0900

    add: Add AddEventPageNotifier

---
+++ lib/presentation/addevent_page/addevent_page_notifier.dart
@@ -0,0 +1,27 @@
+import 'package:expressive_writing/infrastructure/auth_repository.dart';
+import 'package:expressive_writing/infrastructure/event_repository.dart';
+import 'package:expressive_writing/state/addevent_page_state.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AddEventPageNotifier extends StateNotifier<AddEventPageState> {
+  final BaseAuthRepository _authRepository;
+  final EventRepository _eventRepository;
+
+  AddEventPageNotifier({
+    required EventRepository eventRepository,
+    required BaseAuthRepository authRepository,
+  })  : _eventRepository = eventRepository,
+        _authRepository = authRepository,
+        super(AddEventPageState(
+            description: "",
+            wordCount: 0,
+            createdAt: DateTime.now(),
+            madeBy: FirebaseAuth.instance.currentUser!.uid));
+
+  Future<void> setDescription(String description) async {
+    state = state.copyWith(description: description);
+  }
+
+  Future<void> addEvent({required AddEventPageState}) async {}
+}
