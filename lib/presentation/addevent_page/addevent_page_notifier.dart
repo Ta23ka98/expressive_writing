065bcffcdf8803460d0a3762baed0c6309d8a6e2
まさたか <shinonome419@icloud.com>
2022-10-18 07:16:02 +0900

    update: Update setDescription() and addEvent()

--- lib/presentation/addevent_page/addevent_page_notifier.dart
+++ lib/presentation/addevent_page/addevent_page_notifier.dart
@@ -1,13 +1,15 @@
+import 'package:expressive_writing/domain/entity/event.dart';
 import 'package:expressive_writing/infrastructure/auth_repository.dart';
 import 'package:expressive_writing/infrastructure/event_repository.dart';
 import 'package:expressive_writing/state/addevent_page_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-final addEventPageNotifierProvider = StateNotifierProvider((ref) =>
-    AddEventPageNotifier(
-        eventRepository: ref.read(eventRepositoryProvider),
-        authRepository: ref.read(authRepositoryProvider)));
+final addEventPageNotifierProvider =
+    StateNotifierProvider<AddEventPageNotifier, AddEventPageState>((ref) =>
+        AddEventPageNotifier(
+            eventRepository: ref.read(eventRepositoryProvider),
+            authRepository: ref.read(authRepositoryProvider)));
 
 class AddEventPageNotifier extends StateNotifier<AddEventPageState> {
   final BaseAuthRepository _authRepository;
@@ -20,9 +22,20 @@ class AddEventPageNotifier extends StateNotifier<AddEventPageState> {
         _authRepository = authRepository,
         super(AddEventPageState.initial());
 
-  Future<void> setDescription(String description) async {
+  Future<void> setDescriptionAndCount(String description) async {
     state = state.copyWith(description: description);
+    state = state.copyWith(wordCount: description.length);
   }
 
-  Future<void> addEvent({required AddEventPageState}) async {}
+  Future<void> addEvent(
+      {required String description,
+      required int wordCount,
+      required DateTime createdAt,
+      required String madeBy}) async {
+    await _eventRepository.addEvent(
+        description: description,
+        wordCount: wordCount,
+        createdAt: createdAt,
+        madeBy: madeBy);
+  }
 }
