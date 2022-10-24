ceabb4d736b477fe4f2a5cbc18fb44f006a0f65e
まさたか <shinonome419@icloud.com>
2022-10-24 13:12:03 +0900

    update: addEventメソッドにレベルアップ機能を追加

--- lib/presentation/addevent_page/addevent_page_notifier.dart
+++ lib/presentation/addevent_page/addevent_page_notifier.dart
@@ -1,25 +1,32 @@
 import 'package:expressive_writing/domain/entity/event.dart';
 import 'package:expressive_writing/infrastructure/auth_repository.dart';
 import 'package:expressive_writing/infrastructure/event_repository.dart';
+import 'package:expressive_writing/infrastructure/user_repository.dart';
 import 'package:expressive_writing/state/addevent_page_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 final addEventPageNotifierProvider =
-    StateNotifierProvider<AddEventPageNotifier, AddEventPageState>((ref) =>
-        AddEventPageNotifier(
-            eventRepository: ref.read(eventRepositoryProvider),
-            authRepository: ref.read(authRepositoryProvider)));
+    StateNotifierProvider<AddEventPageNotifier, AddEventPageState>(
+  (ref) => AddEventPageNotifier(
+    eventRepository: ref.read(eventRepositoryProvider),
+    authRepository: ref.read(authRepositoryProvider),
+    userRepository: ref.read(userRepositoryProvider),
+  ),
+);
 
 class AddEventPageNotifier extends StateNotifier<AddEventPageState> {
   final BaseAuthRepository _authRepository;
   final EventRepository _eventRepository;
+  final UserRepository _userRepository;
 
   AddEventPageNotifier({
     required EventRepository eventRepository,
     required BaseAuthRepository authRepository,
+    required UserRepository userRepository,
   })  : _eventRepository = eventRepository,
         _authRepository = authRepository,
+        _userRepository = userRepository,
         super(AddEventPageState.initial());
 
   Future<void> setDescriptionAndCount(String description) async {
@@ -37,5 +44,10 @@ class AddEventPageNotifier extends StateNotifier<AddEventPageState> {
         wordCount: wordCount,
         createdAt: createdAt,
         madeBy: madeBy);
+    //await _userRepository.levelUp(id: _authRepository.getUid()!);
+    await _userRepository.LevelUp(
+        id: _authRepository.getUid()!, wordCount: wordCount);
   }
 }
+
+//Future<void> levelUp({required int wordCount}) async {}
